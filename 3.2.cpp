2c5c5bf1c552c03ba1f5129acd420d0ebe401af4
ZergLev <vasilev.sv@phystech.edu>
2023-04-04 11:22:41 +0300

    Tasks 2 and 3 done

---
+++ 3.2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+void SendSms(const string& number, const string& message){
+	cout << "Send '" << message << "' to " << number << endl;
+}
+void SendEmail(const string& email, const string& message){
+	cout << "Send '" << message << "' to " << email << endl;
+}
+class iNotifier{
+	public:
+		virtual void Notify(const string& message) = 0;
+};
+class SmsNotifier{
+	public:
+		SmsNotifier(const string& num) : number(num) {}
+		const string number;
+		void Notify(const string& message) const {
+			SendSms(number, message);
+		}
+};
+class EmailNotifier{
+	public:
+		EmailNotifier(const string& mail) : email(mail) {}
+		const string email;
+		void Notify(const string& message) const {
+			SendEmail(email, message);
+		}
+};
+void Notify(iNotifier& notifier, const string& message){
+	notifier.Notify(message);
+}
+int main(){
+	string number = "8(800)555-35-35";
+	string email = "my_boss@gmail.com";
+	SmsNotifier a(number);
+	EmailNotifier b(email);
+	a.Notify("Where is the money, Lebowski?");
+	b.Notify("A very important message.");
+	return 0;
+}
---
+++ 4.2.2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <sstream>
+#include <utility>
+#include <vector>
+#include <map>
+
+using namespace std;
+/*
+template <typename T, typename U>
+ostream& operator<<(ostream& stream, const pair<T, U>& pair){
+  stream << pair.first << " " << pair.second;
+  return stream;
+}
+*/
+template <typename T>
+ostream& operator<<(ostream& stream, const vector<T>& vector){
+	int size = vector.size();
+	int i = 0;
+	stream << "[";
+	for (auto elem : vector) {
+  	stream << elem;
+		i++; // elements already displayed
+		//if not all elements have been displayed, type a comma and a space
+		if (i < size) 
+			stream << ", ";
+	}
+	stream << "]";
+  return stream;
+}
+template <typename T, typename U>
+ostream& operator<<(ostream& stream, const map<T,U>& map){
+	int size = map.size();
+	int i = 0;
+	stream << "{";
+	for (auto elem : map) {
+  	stream << "{" << elem.first << ", " << elem.second << "}";
+		i++; // elements already displayed
+		//if not all elements have been displayed, type a comma and a space
+		if (i < size) 
+			stream << ", ";
+	}
+	stream << "}";
+  return stream;
+}
+
+int main(){
+	map<int, string> a;
+	vector<int> b;
+	vector<string> c;
+	cout << "Input three key values(integers) for the map." << endl;
+	int array[3];
+	cin >> array[1] >> array[2] >> array[3];
+	cout << "Input three words for the map." << endl;
+  cin >> a[array[1]] >> a[array[2]] >> a[array[3]];
+	int element;
+	cout << "Input five numbers for the vector." << endl;
+	for (int j = 0; j < 5; j++) {
+  	cin >> element;
+		b.push_back(element);
+	}
+	cout << "Input three words for the vector." << endl;
+	string elem;
+	for (int j = 0; j < 3; j++) {
+  	cin >> elem;
+		c.push_back(elem);
+	}
+  cout << a << endl;
+  cout << b << endl;
+  cout << c << endl;
+  return 0;
+}
